8021f3c9966e09ef1b5e5a3a09e189d0f2d8d068
Aiiboo <aiibooDeveloper@gmail.com>
2023-09-24 01:36:36 +0200

    Initial Commit

---
+++ ObserveTest/ObserveTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObserveTestApp.swift
+//  ObserveTest
+//
+//  Created by Ibrahim Ramadan on 24.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ObserveTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
